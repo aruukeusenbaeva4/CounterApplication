777f0e2aa05171eb5eeefaab86c348a3be7f7f7d
aruukeusenbaeva4 <aruuke.usenbaeva4@gmail.com>
2025-10-25 20:33:48 +0600

    рр

--- app/src/main/java/com/example/counterapplication/MainActivity.kt
+++ app/src/main/java/com/example/counterapplication/MainActivity.kt
@@ -10,16 +10,26 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.counterapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var counter=0
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        var counterPref =CounterPref(context = this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.tvText.text= counterPref.getCount().toString()
+
+        binding.btnIncrement.setOnClickListener {
+            counter++
+            binding.tvText.text = counter.toString()
+            counterPref.saveCount(counter)
+
         }
     }
 }
